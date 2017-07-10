7466af69bf67771a807b9417d32cf7e7e8fe6f87
51MARGL <97.pibody.97@gmail.com>
2017-07-10 19:36:11 +0200

    Add files via upload

---
+++ ZP4CS/ex-7/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv_7 {
+    class Program {
+
+        public static void MergeSort (Student[] names) {
+            if (names.Count() >= 2) {
+                Student[] left = new Student[names.Count() / 2];
+                Student[] right = new Student[names.Count() - names.Count() / 2];
+
+                for (int i = 0; i < left.Count(); i++) {
+                    left[i] = names[i];
+                }
+
+                for (int i = 0; i < right.Count(); i++) {
+                    right[i] = names[i + names.Count() / 2];
+                }
+
+                MergeSort(left);
+                MergeSort(right);
+                Merge(names, left, right);
+            }
+        }
+
+        public static void Merge (Student[] names, Student[] left, Student[] right) {
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < names.Count(); i++) {
+                if (b >= right.Count() || (a < left.Count() 
+                    && string.Compare(left[a].Prijmeni, right[b].Prijmeni, StringComparison.CurrentCultureIgnoreCase) < 0)) {
+                    names[i] = left[a];
+                    a++;
+                } else {
+                    names[i] = right[b];
+                    b++;
+                }
+            }
+        }
+
+
+        static void Main (string[] args) {
+
+            MergeSort(ReadonlyDB.Students);
+            foreach (var item in ReadonlyDB.Students) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            List<Student> resultList = new List<Student>();
+            for (int i = 4; i < ReadonlyDB.Students.Count() && i < 15; i++) {
+                resultList.Add(ReadonlyDB.Students[i]);
+            }
+            Console.WriteLine(resultList.Count);
+            for (int i = 0; i < resultList.Count - 1; i++) {
+                if (resultList[i].Jmeno.CompareTo(resultList[i + 1].Jmeno) == 0
+                    && resultList[i].Prijmeni.CompareTo(resultList[i + 1].Prijmeni) == 0) {
+                    resultList.RemoveAt(i + 1);
+                }
+            }
+            Console.WriteLine(resultList.Count);
+            foreach (var item in resultList) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            ///////////
+
+            var sortedS = ReadonlyDB.Students
+                .OrderBy(p => p.Prijmeni)
+                .Skip(4)
+                .Take(10)
+                .GroupBy(p => new { p.Jmeno, p.Prijmeni })
+                .Select(g => g.First());
+
+            foreach (var item in sortedS) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("5. rocnik:");
+            foreach (var item in ReadonlyDB.Students.Where(p => p.Rocnik >= 5)) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("MI counnt= " + ReadonlyDB.Students.Count(p => p.OborKomb == "MI"));
+            Console.WriteLine();
+
+            Console.WriteLine("Zdeněk: " + ReadonlyDB.Students.Any(p => p.Jmeno == "Zdeněk"));
+        }
+    }
+}
---
+++ ZP4CS/ex-7/Student.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv_7 {
+
+    public class Student {
+        public string OsCislo { get; set; }
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public string UserName { get; set; }
+        public int Rocnik { get; set; }
+        public string OborKomb { get; set; }
+
+        public override string ToString () {
+            return $"{OsCislo}, {Jmeno}, {Prijmeni}, {UserName}, {Rocnik}, {OborKomb}";
+        }
+    }
+
+    public class ReadonlyDB {
+        public static Student[] Students = {
+   new Student() {
+      OsCislo="R140347",
+      Jmeno="Zdeněk",
+      Prijmeni="BARTAL",
+      UserName="bartzd03",
+      Rocnik=3,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R15878",
+      Jmeno="Rostislav",
+      Prijmeni="CIBULKA",
+        UserName="ciburo00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15880",
+      Jmeno="Ema",
+      Prijmeni="CSÁKÁNYIOVÁ",
+        UserName="csakem00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15881",
+      Jmeno="Matyáš",
+      Prijmeni="ČERNOHOUS",
+        UserName="cernma21",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15885",
+      Jmeno="Aleš",
+      Prijmeni="DOPITA",
+        UserName="dopitaa",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15887",
+      Jmeno="Martin",
+      Prijmeni="FILIPI",
+        UserName="filima05",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R14316",
+      Jmeno="Daniel",
+      Prijmeni="HÁJEK",
+        UserName="hajeda00",
+        Rocnik=3,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R14315",
+      Jmeno="Daniel",
+      Prijmeni="HÁJEK",
+        UserName="hajeda00",
+        Rocnik=3,
+      OborKomb="MI"}
+      ,
+      new Student() {
+      OsCislo="R150570",
+      Jmeno="Václav",
+      Prijmeni="HLAVINKA",
+        UserName="hlavva00",
+        Rocnik=2,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R15892",
+      Jmeno="Petr",
+      Prijmeni="HOLÁŇ",
+        UserName="holape00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15894",
+      Jmeno="Jan",
+      Prijmeni="HRABAL",
+        UserName="hrabja08",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15895",
+      Jmeno="David",
+      Prijmeni="HRŮZA",
+        UserName="hruzda01",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R140554",
+      Jmeno="Tomáš",
+      Prijmeni="CHLUP",
+        UserName="chluto00",
+        Rocnik=3,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R15369",
+      Jmeno="Albert",
+      Prijmeni="KAUFMAN",
+        UserName="kaufal00",
+        Rocnik=2,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R14913",
+      Jmeno="Patrik",
+      Prijmeni="KRAIF",
+        UserName="kraipa00",
+        Rocnik=3,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15910",
+      Jmeno="Serhiy",
+      Prijmeni="KUDRYASHOV",
+        UserName="kudrse00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15916",
+      Jmeno="Libor",
+      Prijmeni="MACHÁLEK",
+        UserName="machli00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15350",
+      Jmeno="Zdeněk",
+      Prijmeni="MAZURÁK",
+        UserName="mazuzd00",
+        Rocnik=2,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R15922",
+      Jmeno="Zbyněk",
+      Prijmeni="MLČÁK",
+        UserName="mlcazb00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15924",
+      Jmeno="Jan",
+      Prijmeni="MOLČÍK",
+        UserName="molcja00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15351",
+      Jmeno="Adam",
+      Prijmeni="OBRTEL",
+        UserName="obrtad00",
+        Rocnik=2,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R15935",
+      Jmeno="Libor",
+      Prijmeni="POLÁŠEK",
+        UserName="polali00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R14843",
+      Jmeno="Tomáš",
+      Prijmeni="POSPÍŠIL",
+        UserName="pospto05",
+        Rocnik=3,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15944",
+      Jmeno="Radim",
+      Prijmeni="SEKERA",
+        UserName="sekera00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15357",
+      Jmeno="Michaela",
+      Prijmeni="SIEKELOVÁ",
+        UserName="siekmi00",
+        Rocnik=2,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R14856",
+      Jmeno="Michal",
+      Prijmeni="STARIAT",
+        UserName="starmi00",
+        Rocnik=3,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15949",
+      Jmeno="Lukáš",
+      Prijmeni="STŘELECKÝ",
+        UserName="strelu00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15957",
+      Jmeno="František",
+      Prijmeni="TOMAN",
+        UserName="tomafr00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15959",
+      Jmeno="Viet",
+      Prijmeni="TRAN HOANG",
+        UserName="tranvi00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15361",
+      Jmeno="Michal",
+      Prijmeni="VÁCLAVEK",
+        UserName="vaclmi04",
+        Rocnik=2,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R15961",
+      Jmeno="Petr",
+      Prijmeni="VALIGURA",
+        UserName="valipe00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15363",
+      Jmeno="David",
+      Prijmeni="VRBA",
+        UserName="vrbada00",
+        Rocnik=2,
+      OborKomb="MI"}
+      , new Student() {
+      OsCislo="R15968",
+      Jmeno="Tomáš",
+      Prijmeni="ZÁLEŠÁK",
+        UserName="zaleto00",
+        Rocnik=2,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R14874",
+      Jmeno="Michael",
+      Prijmeni="ZAPLETAL",
+        UserName="zaplmi06",
+        Rocnik=3,
+      OborKomb="APLINF"}
+      , new Student() {
+      OsCislo="R15971",
+      Jmeno="Lenka",
+      Prijmeni="ZAPLETALOVÁ",
+        UserName="zaplle02",
+        Rocnik=2,
+      OborKomb="APLINF"}};
+    }
+}
