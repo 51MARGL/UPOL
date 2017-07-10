ff989cae50d90a34d93d08034c5ea02188df0461
51MARGL <97.pibody.97@gmail.com>
2017-07-10 19:24:22 +0200

    Add files via upload

---
+++ ZP4CS/ex-3/PMerge.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cv_3
+{
+    class PMerge
+    {
+        public void ParalMergeSort(int[] A, int p, int r, int depth)
+        {
+            if (p < r && depth > 0)
+            {
+                int q = (p + r) / 2;
+
+                depth--;
+                Parallel.Invoke(
+                    () => ParalMergeSort(A, p, q, depth),
+                    () => ParalMergeSort(A, q + 1, r, depth)
+                    );
+
+                // Jsem precetl, ze je to stejne jako
+                //
+                // Thread t1 = new Thread(() => ParalMergeSort(A, p, q, depth));
+                // Thread t2 = new Thread(() => ParalMergeSort(A, q + 1, r, depth));
+                // t1.Start();
+                // t2.Start();
+                // t1.Join();
+                // t2.Join();
+                //
+                // A stejne jako to
+                //
+                // Task t1 = Task.Run(() => ParalMergeSort(A, p, q, depth));
+                // Task t2 = Task.Run(() => ParalMergeSort(A, q + 1, r, depth));
+                // Task.WaitAll(t1, t2);
+
+
+                Merge(A, p, q, r);
+            }
+            else if (p < r)
+            {
+                int q = (p + r) / 2;
+                SeqMergeSort(A, p, q);
+                SeqMergeSort(A, q + 1, r);
+                Merge(A, p, q, r);
+            }
+        }
+
+        private void SeqMergeSort(int[] A, int p, int r)
+        {
+
+            if (p < r)
+            {
+                int q = (p + r) / 2;
+                SeqMergeSort(A, p, q);
+                SeqMergeSort(A, q + 1, r);
+                Merge(A, p, q, r);
+            }
+        }
+
+        private void Merge(int[] A, int p, int q, int r)
+        {
+            int n1 = q - p + 1;
+            int n2 = r - q;
+            int[] L = new int[n1 + 1];
+            int[] R = new int[n2 + 1];
+            Array.Copy(A, p, L, 0, L.Length - 1);
+            Array.Copy(A, q + 1, R, 0, R.Length - 1);
+            
+            L[n1] = int.MaxValue;
+            R[n2] = int.MaxValue;
+            
+            int n = 0;
+            int m = 0;
+            for (int k = p; k <= r; k++)
+            {
+                if (L[n].CompareTo(R[m]) < 0 || L[n].CompareTo(R[m]) == 0)
+                {
+                    A[k] = L[n];
+                    n++;
+                }
+                else
+                {
+                    A[k] = R[m];
+                    m++;
+                }
+            }
+        }
+    }
+}
---
+++ ZP4CS/ex-3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace cv_3
+{
+    class Program
+    {
+
+        public static void ParalelMergeSort(int[] o, int depth)
+        {
+            PMerge m = new PMerge();
+            m.ParalMergeSort(o, 0, o.Length - 1, depth);
+        }
+
+        public static void PrintArray<T>(T[] t)
+        {
+            foreach (T item in t)
+            {
+                Console.Write($"{item.ToString()}, ");
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] GetRandomArray(int v)
+        {
+            Random r = new Random();
+            int[] arr = new int[v];
+            for (int i = 0; i < v; i++)
+            {
+                arr[i] = r.Next(0, 10000);
+            }
+            return arr;
+        }
+
+        public static bool IsSorted(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr1 = GetRandomArray(10000000);
+            int[] arr2 = new int[arr1.Length];
+            int[] arr3 = new int[arr1.Length];
+            Array.Copy(arr1, arr2, arr1.Length);
+            Array.Copy(arr1, arr3, arr1.Length);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            ParalelMergeSort(arr1, 0);
+            sw.Stop();
+            Console.WriteLine("0 threads: {0}ms; Is Sorted? - {1}", sw.Elapsed.TotalMilliseconds, IsSorted(arr1));
+
+            sw = Stopwatch.StartNew();
+            ParalelMergeSort(arr2, 1);
+            sw.Stop();
+            Console.WriteLine("1 thread: {0}ms; Is Sorted? - {1}", sw.Elapsed.TotalMilliseconds, IsSorted(arr2));
+
+            sw = Stopwatch.StartNew();
+            ParalelMergeSort(arr3, 2);
+            sw.Stop();
+            Console.WriteLine("2 threads: {0}ms; Is Sorted? - {1}", sw.Elapsed.TotalMilliseconds, IsSorted(arr3));
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
