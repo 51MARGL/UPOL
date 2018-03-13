5e2c4bd50b9550470bf2ba3be3556aa13f12bf84
51MARGL <serhiy.kudryashov@gmail.com>
2018-03-13 19:34:04 +0100

    index button fix + PARA first commit

---
+++ PARA/cv1/cv1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace cv1
+{
+    class Program
+    {
+        private static int counter;
+        private static object lockKey;
+        private static void Increment(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                // lock (lockKey)
+                // {
+                counter++;
+                // } 
+            }
+        }
+
+        private static void SleepMe(Random rand)
+        {
+            var time = rand.Next(0, 5000);
+            Thread.Sleep(time);
+            Console.WriteLine("I was sleeping for: " + (double)time / 1000 + " s");
+        }
+
+        static int[] C = { 11, 22, 3, 42, 15, 6, 73, 85, 91, 10 };
+        static int[] D = new int[10];
+
+        private static void ConcA(int i)
+        {
+            var myNumber = C[i];
+            var count = C.Count(c => c < myNumber);
+            D[count] = myNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            lockKey = new object();
+            counter = 0;
+            int n = 100000;
+            Console.WriteLine("Counter increment:");
+            Parallel.Invoke(
+                () => Increment(n),
+                () => Increment(n)
+                );
+            Console.WriteLine("Counter = " + counter);
+
+            Console.WriteLine("Thread sleeping:");
+            var rand = new Random();
+            var t1 = Task.Run(() => SleepMe(rand));
+            var t2 = Task.Run(() => SleepMe(rand));
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine("Conc alg A:");
+            var taskList = new List<Thread>();
+            for (var i = 0; i < 10; i++)
+            {
+                int index = i;
+                var t = new Thread(() => ConcA(index));
+                t.Start();
+                taskList.Add(t);
+            }
+            taskList.ForEach(t => t.Join());
+            Console.WriteLine("D:");
+            foreach (var i in D)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PARA/cv2/cv2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cv2
+{
+    class Program
+    {
+        private static Tuple<int, int> pair;
+        private static int? resultGCD;
+        private static int oneCount;
+        private static int allCount;
+
+        private static bool exit;
+
+        static void Producent()
+        {
+            Random rand = new Random();
+            int x1 = rand.Next(1, 1001);
+            int x2 = rand.Next(1, 1001);
+            pair = new Tuple<int, int>(x1, x2);
+            while (!exit)
+            {
+                x1 = rand.Next(1, 1001);
+                x2 = rand.Next(1, 1001);
+                while (pair != null) if (exit) return;
+                pair = new Tuple<int, int>(x1, x2);
+            }
+        }
+
+        private static void ProdCons()
+        {
+            while (!exit)
+            {
+                while ( pair == null || resultGCD != null) if (exit) return;
+                resultGCD = GCD(pair.Item1, pair.Item2) > 1 ? 1 : 0;
+                pair = null;
+            }
+        }
+
+        private static void Consument()
+        {
+            while (number > oneCount)
+            {
+                while (resultGCD == null) ;
+                if (resultGCD == 1)
+                    oneCount++;
+                allCount++;
+                resultGCD = null;
+            }
+            exit = true;
+        }
+
+        private static int GCD(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a == 0 ? b : a;
+        }
+
+        private static int number;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            var threadList = new List<Thread> { new Thread(Producent), new Thread(ProdCons), new Thread(Consument) };
+
+            threadList.ForEach(t => t.Start());
+            threadList.ForEach(t => t.Join());
+            Console.WriteLine("All count = " + allCount);
+            Console.WriteLine("Ones count = " + oneCount);
+            Console.WriteLine("Division = " + (float)oneCount / allCount);
+
+            Console.ReadLine();
+        }
+    }
+}
