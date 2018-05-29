babea0e17cf5de82abf408fb9f869b897ff7ee7a
51MARGL <97.pibody.97@gmail.com>
2018-05-29 15:00:46 +0200

    done

--- INFS/connection.php
+++ INFS/connection.php
@@ -13,7 +13,9 @@ class Db
     {
         if (!isset(self::$instance)) {
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            self::$instance = new PDO('mysql:host=localhost;dbname=ExpressDB', 'root', '***', $pdo_options);
+            $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
+                 
+            self::$instance = new PDO('mysql:host=localhost;dbname=ExpressDB', 'root', '', $pdo_options);
         }
         return self::$instance;
     }
